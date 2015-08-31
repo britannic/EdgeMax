30d1e2fdde99c5bb4730db297d690c7ecc51520c
britannic <github@empirecreekcircle.com>
2015-08-31 15:48:28 -0700

    UBNT EdgeMax Blacklist and Ad Server Blocking
    * Generates a dnsmasq configuration file that can be used directly by
    dnsmasq
    * Any fqdn in the blacklist will return the IP address configured in
    the update-blacklists-dnsmasq.pl script at line 15
    * Exclusions can be added in line 22

---
+++ Blacklist Blocking/README.md
@@ -0,0 +1,32 @@
+# UBNT EdgeMax Blacklist and Ad Server Blocking
+https://community.ubnt.com/t5/EdgeMAX/Self-Installer-to-configure-Ad-Server-and-Blacklist-Blocking/td-p/1337892
+git@empirecreekcircle.com
+@britannic
+
+NOTE: THIS IS NOT OFFICIAL UBIQUITI SOFTWARE AND THEREFORE NOT SUPPORTED OR ENDORSED BY Ubiquiti Networks®
+
+## Overview
+EdgeMax Blacklist and Ad Server Blocking is derived from the received wisdom found at (https://community.ubnt.com/t5/EdgeMAX/bd-p/EdgeMAX)
+
+## Licenses
+* GNU General Public License, version 3
+* GNU Lesser General Public License, version 3
+
+## Features
+* Generates a dnsmasq configuration file that can be used directly by dnsmasq
+* Any fqdn in the blacklist will return the IP address configured in the update-blacklists-dnsmasq.pl script at line 15
+* Exclusions can be added in line 22
+
+## Compatibility
+*  update-blacklists-dnsmasq.pl has been tested on the EdgeRouter Lite family of routers, version v1.6.0-v1.7.0.
+* Since the EdgeOS is a fork and port of Vyatta 6.3, this script could easily be adapted for work on VyOS and Vyatta derived ports
+
+## Installation
+
+
+upload ersetup.tgz to your router (e.g. scp <local path>/ersetup.tgz <user>@<erl router>:/tmp/ersetup.tgz
+on your router: cd /tmp; sudo tar zxvf /tmp/ersetup.tgz
+sudo bash /tmp/ersetup.tgz
+optional: update line 15 of /config/scripts/update-blacklists-dnsmasq.pl wih your pixelserver IP
+optional: update line 22 with any desired exclusion addresses (i.e. ".*googleadservices.com", ".*hulu.com")
+kickstart the blacklist update (cron will run it at midnight local time): /config/scripts/update-blacklists-dnsmasq.pl
---
+++ Blacklist Blocking/payload/update-blacklists-dnsmasq.pl
@@ -0,0 +1,85 @@
+#!/usr/bin/env perl
+# This script will write a combined, unique sorted list of adserver and
+# blacklisted fqdns to a file in dnsmasq format
+#
+use warnings;
+use strict;
+use integer;
+
+my $ad_list_url    = "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro=0&mimetype=plaintext";
+my $blacklist_url  = "http://winhelp2002.mvps.org/hosts.txt";
+my $dnsmasq        ="/etc/init.d/dnsmasq";
+
+# The IP address below should point to the IP of your router/pixelserver or to 0.0.0.0
+# 0.0.0.0 is easy and doesn't require much from the router
+my $black_hole_ip  = "0.0.0.0";
+my $blacklist_file = "/etc/dnsmasq.d/dnsmasq.blacklist.conf";
+my @blacklist;
+my @adlist;
+
+# Add any exclusions here (i.e. Hulu, Google lead services). Note, in order to
+# use regex matching, prepend the domain or fqdn with '.*' - see examples below
+my $exclusions     = (join("|", ".*googleadservices.com",".*hulu.com"));
+
+sub uniq {
+    my %hash       = map { $_ => 1 } @_;
+    return keys %hash;
+}
+
+sub write_list {
+    my $fh;
+    my $file       = shift;
+    my @list       = @_;
+    open( $fh, '>', $file ) || die "Could not open file: '$file' $!";
+    print $fh ( @list );
+    close($fh);
+}
+
+sub update_adlist {
+
+    # Get ad block list
+    foreach my $line (qx(curl -s $ad_list_url)) {
+        chomp $line;
+        if ( $line =~ /^address/ ) {
+            if ($line !~ /$exclusions/){
+                push @adlist,
+                lc( $line
+                   =~ s|^(address=/.*)(/127\.0\.0\.1)|$1/$black_hole_ip\n|r
+                );
+            }
+        }
+    }
+}
+
+sub update_blacklist {
+
+# Get blacklist and convert the hosts file into a dnsmasq.conf format
+# file. Be paranoid and replace every IP address with $black_hole_ip.
+# We only want the actual blacklist, so we can prepend our own hosts.
+# $black_hole_ip="0.0.0.0" saves router CPU cycles and is more efficient
+
+    foreach my $line (qx(curl -s $blacklist_url)) {
+        chomp $line;
+        if ( $line =~ /^0\.0\.0\.0.[-a-z0-9.]*/ ) {
+            if ($line !~ /$exclusions/){
+                push @blacklist,
+                lc( $line
+                   =~ s|^(0\.0\.0\.0\s)([-a-z0-9_.]*)(.*)|address=/$2/$black_hole_ip\n|r
+                );
+            }
+        }
+    }
+}
+
+sub get_blacklist {
+    update_adlist;
+    update_blacklist;
+    push @blacklist, @adlist;
+    return sort( uniq(@blacklist) );
+}
+
+# print get_blacklist;
+write_list( $blacklist_file, get_blacklist() ) && system ("$dnsmasq force-reload");
+
+
+
